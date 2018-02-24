71c3ac04114b82a437f19f19bdd31e15a4e9d815
Lenovo <Lenovo@DESKTOP-3F2Q1UO>
2018-02-24 21:00:02 +0100

    rebuild

--- src/controllers/sunburst/ColorStrategyDecoratorRedSelected.java
+++ src/controllers/sunburst/ColorStrategyDecoratorRedSelected.java
@@ -13,27 +13,17 @@ import javafx.scene.paint.Color;
  * @author Seweryn
  */
 public class ColorStrategyDecoratorRedSelected extends IColorStrategyDecorator {
-    
-  
-
     public ColorStrategyDecoratorRedSelected(IColorStrategy iColorStrategy) {
         super(iColorStrategy);
-
     }
 
     @Override
     public Color colorFor(WeightedTreeItem<Node> item, int sector, int level) {
         Color tempColor = iColorStrategy.colorFor(item,sector,level);
-        
             if(item.getValue().getSelected()){
                 tempColor = Color.RED;
             }
-                
-        
         return tempColor;
-                
     }
 
-    
-    
 }
